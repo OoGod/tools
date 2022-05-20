cd55f618ff663ae5db7a96305663a7f41bd15d07
OoGod <1617605689@qq.com>
2022-05-21 00:42:11 +0800

    update script

--- test_lib/gen_ssh_key.sh
+++ test_lib/gen_ssh_key.sh
@@ -2,3 +2,6 @@ email=${1}
 ssh-keygen -t rsa -N '' -C ${email} -f ~/.ssh/id_rsa_github -q
 echo 'ssh public key: '
 cat ~/.ssh/id_rsa_github.pub
+#https://blog.csdn.net/qq_43634735/article/details/121197352
+ssh-agent -s
+ssh-add ~/.ssh/id_rsa_github
